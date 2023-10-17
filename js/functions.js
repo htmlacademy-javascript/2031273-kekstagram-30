ef51cfe611e71e57612f1851fd6c40f8de6afbe5
schwarz1k <dlinkis39@mail.ru>
2023-10-17 20:11:26 +0300

    Точечно отключает eslint

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,14 @@
+// Проверка длины строки
 function checkStringLength(string, maxLength) {
   return string.length <= maxLength;
 }
 
+/* eslint-disable no-console */
 console.log(checkStringLength('проверяемая строка', 20));
 console.log(checkStringLength('проверяемая строка', 18));
 console.log(checkStringLength('проверяемая строка', 11));
 
-
+// Проверка, является ли строка палиндромом
 function palindrome(str) {
   str = str.toLowerCase().replace(/ /g, '');
   return str.split('').reverse().join('') === str;
@@ -16,4 +18,3 @@ console.log(palindrome('топот'));
 console.log(palindrome('ДовОд'));
 console.log(palindrome('Кекс'));
 console.log(palindrome('Лёша на полке клопа нашёл'));
-
