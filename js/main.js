8746a81383efaccff716cd05a31b6df8ce9ee9c9
schwarz1k <dlinkis39@mail.ru>
2023-10-28 17:25:04 +0300

    делает правки по комментариям

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import { generatePhotos } from './data.js';
+import('./thumbnailRenderer.js').then((module) => {
+  const { renderThumbnails } = module;
+  renderThumbnails();
+});
 
 /* eslint-disable no-console */
 const photosArray = generatePhotos();
--- js/thumbnailRenderer.js
+++ js/thumbnailRenderer.js
@@ -1,33 +1,27 @@
 import { generatePhotos } from './data.js';
 
-function createThumbnail(photo) {
+const createThumbnail = (photo) => {
   const template = document.querySelector('#picture');
   const thumbnail = template.content.cloneNode(true);
-
   const img = thumbnail.querySelector('.picture__img');
   img.src = photo.url;
   img.alt = photo.description;
-
   const likesCount = thumbnail.querySelector('.picture__likes');
   likesCount.textContent = photo.likes;
-
   const commentsCount = thumbnail.querySelector('.picture__comments');
   commentsCount.textContent = photo.comments.length;
-
   return thumbnail;
-}
+};
 
-function renderThumbnails() {
+const renderThumbnails = () => {
   const photos = generatePhotos();
-
   const fragment = document.createDocumentFragment();
   photos.forEach((photo) => {
     const thumbnail = createThumbnail(photo);
     fragment.appendChild(thumbnail);
   });
-
   const picturesContainer = document.querySelector('.pictures');
   picturesContainer.appendChild(fragment);
-}
+};
 
-renderThumbnails();
+export { renderThumbnails };
