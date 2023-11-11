4046d07c533a64ed0f58847d2f4edf304a0f8313
schwarz1k <125962490+schwarz1k@users.noreply.github.com>
2023-11-11 15:23:57 +0300

    Правда или действие (#8)
    * приступает к дз №9
    * Реализовал возможность открытия и закрытия формы загрузки изображения и сделал валидацию
    
    ---------
    
    Co-authored-by: Дмитрий Линкис <schwarz1k@MBP-Dmitrij.lan>

---
+++ js/form.js
@@ -0,0 +1,37 @@
+function getElement(selector) {
+  return document.querySelector(selector);
+}
+
+const imageInput = getElement('.img-upload__input');
+const imageUpload = getElement('.img-upload__overlay');
+const closeImageButton = getElement('.img-upload__cancel');
+
+const toggleClasses = (isOpen = true) => {
+  imageUpload.classList.toggle('hidden', !isOpen);
+  document.body.classList.toggle('modal-open', isOpen);
+};
+
+const onOpenImageUpload = () => {
+  toggleClasses();
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const onCloseImageUpload = () => {
+  imageInput.value = '';
+  toggleClasses(false);
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+function onDocumentKeydown(evt) {
+  const isForm =
+    document.activeElement === getElement('.text__hashtags') ||
+    document.activeElement === getElement('.text__description');
+
+  if (evt.key === 'Escape' && !isForm) {
+    evt.preventDefault();
+    onCloseImageUpload();
+  }
+}
+
+imageInput.addEventListener('change', onOpenImageUpload);
+closeImageButton.addEventListener('click', onCloseImageUpload);
